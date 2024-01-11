94ee1ea74c4a2968ad08230f2f0057ccb9bc054c
Max N <account@account.local>
2024-01-11 01:46:08 -0800

    start spark use

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pyspark.sql import SparkSession
+import config
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    spark = SparkSession.builder.appName('DE_postings').getOrCreate()
+    df = spark.read.csv(config.DATA_PATH, header=True)
+    df.show(5)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
